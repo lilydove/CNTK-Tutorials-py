87ec903cbc568374287a436dcfbed713353e33c5
Yee Wu <wuyiwangyi@163.com>
2018-05-23 13:23:03 +0800

    CNTK203 2018-05-22
    DL

--- CNTK-203-RL.py
+++ CNTK-203-RL.py
@@ -9,6 +9,8 @@ from matplotlib import style
 import numpy as np
 import pandas as pd 
 import seaborn as sns
+import cntk as C
+import os
 
 try:
     import gym
@@ -20,7 +22,24 @@ style.use('ggplot')
 Image(url="https://cntk.ai/jup/polecart.git", width=300, height=300)
 
 isFast = True
+# Select the right target device when this notebook is being tested:
+if 'TEST_DEVICE' in os.environ:
+    if os.environ['TEST_DEVICE'] == 'cpu':
+        C.device.try_set_default_device(C.device.cpu())
+    else:
+        C.device.try_set_default_device(c.device.gup(0))
 
+env = gym.make('CartPole-v0')
+
+STATE_COUNT = env.observation_space.shape[0]
+ACTION_COUNT = env.action_space.n
+
+# Targetted reward
+REWARD_TARGET = 30 if isFast else 200
+# Averaged over these these many episodes
+BATCH_SIZE_BASELINE = 20 if isFast else 50
+
+H = 64 # hidden layer size
 # ------1.Data------
 
 
